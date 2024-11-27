bbd46cad6e640c8e91460bd2aa6b61a8cea1b288
DanilDV <Pers1978@gmail.com>
2024-11-27 21:36:34 +0300

    update vs

--- obj/Debug/net8.0/UserManagementApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/UserManagementApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagementApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b0f4c29af62f8341142ca8ae75b577209f38107")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagementApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagementApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
